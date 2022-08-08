3b02baaac6f6b458c01b1cc68056c7963dcf10aa
LucasPascale <lpluke5@gmail.com>
2022-08-08 10:20:34 -0500

    gemsetup/spec_setup: something went wrong and I had to re bundle all my gems

--- Gemfile
+++ Gemfile
@@ -16,19 +16,24 @@ gem 'jsonapi-serializer'
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
+  gem 'rspec-rails'
   gem 'factory_bot_rails'
-  gem 'faker'
+  gem 'faker' 
+  gem 'capybara'
+  gem 'vcr'
+  gem 'webmock'
+  gem 'shoulda-matchers'
+  gem 'simplecov'
 end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
+  
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
 group :test do
-  gem 'rspec-rails'
-  gem 'webmock'
-  gem 'shoulda-matchers'
-  gem 'simplecov'
   # gem 'database_cleaner-active_record'
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'simplecov'
-SimpleCov.start
 require 'webmock/rspec'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -36,7 +34,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -64,10 +61,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
+end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
 end
