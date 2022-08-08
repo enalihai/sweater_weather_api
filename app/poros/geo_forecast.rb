794640a4794f82b177582f4f0bf256eb75618383
LucasPascale <lpluke5@gmail.com>
2022-08-08 13:40:45 -0500

    app/poro: update poro again

--- app/poros/geo_forecast.rb
+++ app/poros/geo_forecast.rb
@@ -1,10 +1,7 @@
 class GeoForecast
   attr_reader :current_outlook,
               :hourly_outlook,
-              :daily_outlook,
-              :current_weather,
-              :hourly_weather,
-              :daily_weather
+              :daily_outlook
 
   def initialize(data)
     @current_outlook = CurrentForecast.new(data[:current])
