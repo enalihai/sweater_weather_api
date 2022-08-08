3906d84d9fd6194e79ba966d338efa6f8397a480
LucasPascale <lpluke5@gmail.com>
2022-08-08 12:08:03 -0500

    spec/facades/yelp: Add more tests to the response from .search_for_business

--- spec/facades/yelp_facade_spec.rb
+++ spec/facades/yelp_facade_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe YelpFacade do
 
     yelp_search = YelpService.search_for_business(search, location)
     expect(yelp_search).to be_a Hash
+    expect(yelp_search).to have_key :businesses
+    expect(yelp_search[:businesses][0]).to have_key :name
+    expect(yelp_search[:businesses][0]).to have_key :location
+
+    business_location = yelp_search[:businesses][0][:location]
+    expect(business_location).to have_key :address_1
+    expect(business_location).to have_key :address_2
+    expect(business_location).to have_key :city
+    expect(business_location).to have_key :state
+    expect(business_location).to have_key :zip_code
   end
 end
