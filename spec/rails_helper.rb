1839f326ae47a478bfa5c6ed1e5691c6372317b9
LucasPascale <lpluke5@gmail.com>
2022-08-07 21:52:22 -0500

    spec/requests: Add Forecast requests spec files and setup User request spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'simplecov'
-Simplecov.start
-
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -36,7 +33,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -65,7 +62,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
