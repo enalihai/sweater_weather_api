88730abccad1cbe6b26bded9286bc41f2a9445d6
LucasPascale <lpluke5@gmail.com>
2022-08-08 11:31:07 -0500

    still just dealing with this broken API key

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,7 +1,7 @@
 class YelpService
   def self.search_for_business(search, time, location)
     response = conn.get("/v3/businesses/search?term=#{search}&open_at=#{time}&location=#{location}") do |f|
-      f.headers['Authorization'] = ENV['yelp_key']
+      f.headers['Authorization: Bearer'] = ENV['yelp_api_key']
     end
 
     JSON.parse(response.body, symbolize_names: true)
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe YelpService do
-  it 'gets a good response' do
+  it 'gets a good response', :vcr do
     response = YelpService.conn
     expect(response).to be_a(Faraday::Connection)
   end
   
   it 'has data to parse in response', :vcr do
-    response = YelpsService.search_for_business('chinese', Time.now.to_i, "denver")
+    response = YelpService.search_for_business('chinese', Time.now.to_i, "denver")
     expect(response).to be_a Hash
     expect(response).to have_key :businesses
     expect(response).to have_key :total
