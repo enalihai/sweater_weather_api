dee9a21167e74d07662e344c0b05d2cc039228b7
LucasPascale <lpluke5@gmail.com>
2022-08-07 22:20:27 -0500

    spec/services: Setup location/weather service specs and begin build

---
+++ spec/services/location_service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe LocationService do
+  # use mapquest api, but going to use mock / stubbing  for most of my api requests
+end 
---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  # use openweathermap api, but going to use mock / stubbing  for most of my api requests
+end 
