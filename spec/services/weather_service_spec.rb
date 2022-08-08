b388f5f86d6dcda732d026b085344ea461cbd5db
LucasPascale <lpluke5@gmail.com>
2022-08-08 02:07:06 -0500

    spec/services: Update Weather stub so that it works properly

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WeatherService do
   it 'returns a forecast' do
-    openweather_query = {
-      lat: 36.166687,
-      lon: -86.779932,
-      units: 'imperial',
-      exclude: 'minutely'
-    }
     nashville_response = File.read('spec/fixtures/nashville_openweather_response.json')
 
     stub_request(:get, "https://api.openweathermap.org/data/3.0/onecall/?appid=f57e28372b23bcdc9e27700bd3fdd29a&exclude=minutely&lat=36.166687&lon=-86.779932&units=imperial")
-      .with(
-            headers: {
+      .with(headers: {
        	        'Accept'=>'*/*',
        	        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	        'User-Agent'=>'Faraday v2.4.0'
