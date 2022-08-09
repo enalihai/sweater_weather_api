ca64b1466ca75347c37af7e3c3aa6d128797efbb
LucasPascale <lpluke5@gmail.com>
2022-08-09 16:14:38 -0500

    app/user: added bang to end of create because it seemed right and works

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ module Api
     class UsersController < ApplicationController
       def create
         if params[:password] == params[:password_confirmation]
-          user = User.create(email: params[:email], password: params[:password])
+          user = User.create!(email: params[:email], password: params[:password])
           
           render json: UserSerializer.create_user(user), status: 201
         else
