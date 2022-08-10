82dd45ee88ee9e9134d34b220fef6ebc66771c7b
LucasPascale <lpluke5@gmail.com>
2022-08-10 01:02:49 -0500

    spec/sadpath: Add sadpath testing to User

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Sessions', type: :request do
   describe 'GET /index' do
     it 'can create a new session for a user', :vcr do
-      user = User.create!(email: 'user_spec@email.com', 
-                          password: 'password', 
+      user = User.create!(email: 'user_spec@email.com',
+                          password: 'password',
                           password_confirmation: 'password')
-      headers = { 'CONTENT_TYPE'=>'application/json', 
-                  'Accept'=>'application/json'
-                }
+      headers = { 'CONTENT_TYPE' => 'application/json',
+                  'Accept' => 'application/json' }
       session_test = {
         'email': 'user_spec@email.com',
         'password': 'password'
@@ -25,19 +24,19 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
 
       expect(session).to have_key :data
       expect(session[:data]).to be_a Hash
-      
+
       expect(session[:data]).to have_key :type
       expect(session[:data][:type]).to eq 'users'
 
-      expect(session[:data]).to have_key :id 
-      expect(session[:data][:id]).to be_a Integer 
-      
-      expect(session[:data]).to have_key :attributes 
+      expect(session[:data]).to have_key :id
+      expect(session[:data][:id]).to be_a Integer
+
+      expect(session[:data]).to have_key :attributes
       expect(session[:data][:attributes]).to be_a Hash
-      
+
       expect(session[:data][:attributes]).to have_key :email
       expect(session[:data][:attributes][:email]).to be_a String
-      
+
       expect(session[:data][:attributes]).to have_key :api_key
       expect(session[:data][:attributes][:api_key]).to be_a String
 
@@ -45,12 +44,11 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
     end
 
     it 'returns error for invalid user credentials', :vcr do
-      user = User.create!(email: 'user_spec@email.com', 
-                          password: 'password', 
+      user = User.create!(email: 'user_spec@email.com',
+                          password: 'password',
                           password_confirmation: 'password')
-      headers = { 'CONTENT_TYPE'=>'application/json', 
-                  'Accept'=>'application/json'
-                }
+      headers = { 'CONTENT_TYPE' => 'application/json',
+                  'Accept' => 'application/json' }
       session_test = {
         'email': 'user_spec@email.com',
         'password': 'invalid'
@@ -64,13 +62,13 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
 
       expect(session).to have_key :data
       expect(session[:data]).to be_a Hash
-      
+
       expect(session[:data]).to have_key :error
       expect(session[:data][:error]).to eq 'login'
 
       expect(session[:data]).to have_key :message
-      expect(session[:data][:message]).to eq 'invalid credentials' 
-      
+      expect(session[:data][:message]).to eq 'invalid credentials'
+
       expect(session[:data]).to_not have_key :type
       expect(session[:data]).to_not have_key :id
       expect(session[:data]).to_not have_key :attributes
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User Request' do
-  it 'POST /api/v1/users : posts user to db' do
+  it 'POST /api/v1/users : posts user to db', :vcr do
     headers = { 'content_type' => 'application/json', 'Accept' => 'application/json' }
     new_user = {  email: 'somenewuser@email.com',
                   password: 'abc123',
@@ -33,5 +33,25 @@ RSpec.describe 'User Request' do
 
     expect(post_outcome[:data][:attributes]).to have_key :api_key
     expect(post_outcome[:data][:attributes][:api_key]).to be_a String
+  end
+
+   it '#EDGECASE POST /api/v1/users : renders error on bad creation', :vcr do
+    headers = { 'content_type' => 'application/json', 'Accept' => 'application/json' }
+    invalid_input = { email: 'somenewuser@email.com',
+                      password: '123abc',
+                      password_confirmation: 'abc123' }
+
+    post '/api/v1/users', headers: headers, params: invalid_input
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status 404
+
+    post_outcome = JSON.parse(response.body, symbolize_names: true)
+
+    expect(post_outcome).to have_key :error
+    expect(post_outcome[:error]).to eq 'registration'
+    
+    expect(post_outcome).to have_key :message
+    expect(post_outcome[:message]).to eq 'invalid registration'
   end
 end
