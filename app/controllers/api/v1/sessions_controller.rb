fe1bf92c7a75fb555e6fef5cbe0d7f9ce7f6100c
LucasPascale <lpluke5@gmail.com>
2022-08-10 04:52:30 -0500

    app/controllers: Update sad path output for controllers

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < ApplicationController
       session[:user_id] = user.id
       render json: UserSerializer.serialize_user(user), status: 200
     else
-      render json: {data: {error: 'login', message: 'invalid credentials'}}, status: 401
+      render json: { data: { error: 'login', message: 'invalid credentials' } }, status: 401
     end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,22 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def create
-        if params[:password] == params[:password_confirmation]
-          user = User.create!(email: params[:email], password: params[:password])
+      before_action :confirm_password
 
-          render json: UserSerializer.serialize_user(user), status: 201
-        else
-          render json: { error: 'registration', message: 'invalid user attribtues' }, status: 404
+      def create
+        user = User.create!(email: params[:email], password: params[:password])
+
+        render json: UserSerializer.serialize_user(user), status: 201
+      end
+
+      private
+
+      def confirm_password
+        if params[:password] != params[:password_confirmation]
+          render json: { data: { error: 'registration', message: 'password and confirmation must match!' } }, status: 404
         end
       end
+
     end
   end
 end
