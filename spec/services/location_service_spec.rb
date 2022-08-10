bece5c6d041ca156b5cc1cf5cbafaa1f5d08ac09
LucasPascale <lpluke5@gmail.com>
2022-08-10 09:09:41 -0500

    spec/poros: Roadtrip is setup. Got rid of other poro

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -24,22 +24,22 @@ RSpec.describe LocationService do
   end
 
   it 'uses MapQuest API to return a coordinate' do
-    response = LocationService.get_directions('Denver,CO', 'Estes Park,CO')
-    binding.pry
+    response = LocationService.directions('Denver,CO', 'Estes Park,CO')
+    
     expect(response).to be_a Hash
-    expect(response).to have_key :results
-    expect(response[:results]).to be_a Array
+    expect(response).to have_key :route
+    expect(response[:route]).to be_a Hash
 
-    expect(response[:results][0]).to have_key :locations
-    expect(response[:results][0][:locations]).to be_a Array
-    expect(response[:results][0][:locations][0]).to have_key :latLng
+  #   expect(response[:results][0]).to have_key :locations
+  #   expect(response[:results][0][:locations]).to be_a Array
+  #   expect(response[:results][0][:locations][0]).to have_key :latLng
 
-    expect(response[:results][0][:locations][0][:latLng]).to have_key :lat
-    expect(response[:results][0][:locations][0][:latLng][:lat]).to be_a Float
+  #   expect(response[:results][0][:locations][0][:latLng]).to have_key :lat
+  #   expect(response[:results][0][:locations][0][:latLng][:lat]).to be_a Float
 
-    expect(response[:results][0][:locations][0][:latLng]).to have_key :lng
-    expect(response[:results][0][:locations][0][:latLng][:lng]).to be_a Float
+  #   expect(response[:results][0][:locations][0][:latLng]).to have_key :lng
+  #   expect(response[:results][0][:locations][0][:latLng][:lng]).to be_a Float
 
-    expect(response[:results][0][:locations][0][:latLng]).to eq({ lat: 36.166687, lng: -86.779932 })
+  #   expect(response[:results][0][:locations][0][:latLng]).to eq({ lat: 36.166687, lng: -86.779932 })
   end
 end
